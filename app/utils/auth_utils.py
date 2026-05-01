9d657735435aa03d77bfba1bedfe91014d2e4281
himanshugupta0041 <hg5079424@gmail.com>
2026-05-01 18:31:10 +0530

    Update auth_utils.py

--- app/utils/auth_utils.py
+++ app/utils/auth_utils.py
@@ -1,15 +1,15 @@
-from passlib.context import CryptContext
+from jose import jwt
+from datetime import datetime, timedelta
+from typing import Optional
+from ..config import settings
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
 
-
-def verify_password(plain_password: str, hashed_password: str) -> bool:
-    # ensure string + safe length
-    plain_password = str(plain_password)[:72]
-    return pwd_context.verify(plain_password, hashed_password)
-
-
-def get_password_hash(password: str) -> str:
-    # ensure string + safe length
-    password = str(password)[:72]
-    return pwd_context.hash(password)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    return encoded_jwt
